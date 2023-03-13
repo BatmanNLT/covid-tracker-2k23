6f9f21e28f18d21f55e257878ee1dde8ce7e3793
NLT <Neila.Lekane-Tamba@adesso.de>
2023-03-13 18:38:15 +0100

    Change Material Design theme Finish login component Prepare signUp component

--- frontend/src/app/core/components/login/login.component.ts
+++ frontend/src/app/core/components/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  userNameFormControl = new FormControl('', [Validators.required]);
+  pwdFormControl = new FormControl('', [Validators.required]);
 
+  isFormValid() {
+    return (
+      this.pwdFormControl.hasError('required') ||
+      this.userNameFormControl.hasError('required')
+    );
+  }
+
+  onLogin() {
+    console.log('Dummy Login');
+  }
 }
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { CoreMaterialFeaturesModule } from './core.material.features.module';
 import { CoreRoutingModule } from './core.routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [CommonModule, CoreRoutingModule, CoreMaterialFeaturesModule],
+  declarations: [LoginComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    CoreMaterialFeaturesModule,
+    ReactiveFormsModule,
+  ],
 })
 export class CoreModule {}
